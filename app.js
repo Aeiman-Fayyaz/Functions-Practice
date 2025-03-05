98b53f8d485864a4cfb695c5df98e862b6445553
Aeiman Fayyaz <aeimanfayyaz2002@gmail.com>
2025-03-05 12:11:14 +0500

    code update

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 // Store date and time
 
 
-function dateTime() {
-    let time = new Date()
-    let currentDate = new Date().getDate()
-    let currentTime = new Date().getHours()
-    let currentMinute = new Date().getMinutes()
+// function dateTime() {
+//     let time = new Date()
+//     let currentDate = new Date().getDate()
+//     let currentTime = new Date().getHours()
+//     let currentMinute = new Date().getMinutes()
    
-    document.write("Time:"+ "<br/>" +time + "<br/>" + "Today's Date" + "<br/>" + currentDate + "<br/>" + "Current time:"+ "<br/>" + currentTime + ":" + currentMinute)
+//     document.write("Time:"+ "<br/>" +time + "<br/>" + "Today's Date" + "<br/>" + currentDate + "<br/>" + "Current time:"+ "<br/>" + currentTime + ":" + currentMinute)
+// }
+// dateTime();
+
+let userFirstName = prompt("Enter Your First Name")
+let userLastName = prompt("Enter Your Last Name")
+
+function userGreeting(userFirstName , userLastName){
+    alert("Good Morning" + " " + userFirstName + " " + userLastName)
 }
-dateTime();
+userGreeting(userFirstName,userLastName)
+
